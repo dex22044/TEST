2a1f6267e805c5edb92c37f3d4144881266a4914
dex22044 <dex22044@yandex.ru>
2020-01-21 17:51:27 +0300

    Series4

---
+++ Series4/Series4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Series4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res1 = 0,res2 = 1;
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int x= Convert.ToInt32(Console.ReadLine());
+                res1 += x;
+                res2 *= x;
+            }
+            Console.WriteLine(res1);
+            Console.WriteLine(res2
+                );
+        }
+    }
+}
