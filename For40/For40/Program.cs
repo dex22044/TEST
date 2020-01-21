1a1c5953da9651e6e8c2e59023a790833586295d
dex22044 <dex22044@yandex.ru>
2020-01-21 17:18:46 +0300

    Сделал For40
    Сделал For40 (Логично)

---
+++ For40/For40/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace For40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (!(a < b))
+            {
+                Console.WriteLine("Error: A >= B");
+                return;
+            }
+
+            int x = 1;
+
+            for(int i = a; i < b; i++)
+            {
+                for (int d = 0; d < x; d++) Console.WriteLine(i);
+                x++;
+            }
+        }
+    }
+}
