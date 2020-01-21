08f8602bb2580212109ba4a500f0a9b68352b2b6
dex22044 <dex22044@yandex.ru>
2020-01-21 17:41:04 +0300

    Series1

---
+++ Series1/Series1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Series1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res=0;
+            for(int i = 0; i < 10; i++)
+            {
+                res += Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
