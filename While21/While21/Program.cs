2b591341323c519c09d51d943e150a14cee60754
dex22044 <dex22044@yandex.ru>
2020-01-21 17:35:28 +0300

    Сделал While21

---
+++ While21/While21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace While21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            bool result=false;
+            int numberNum = 1, numC = 1;
+            double res=1;
+            while (res >= 1)
+            {
+                res = n / Math.Pow(10,numC);
+                numC++;
+            }
+            while (numberNum < numC && !result)
+            {
+                int num = (int)(n % Math.Pow(10, numberNum)/ Math.Pow(10, numberNum-1));
+                if ((num % 2) == 1) result = true;
+                numberNum++;
+            }
+            if (result) Console.WriteLine("TRUE");
+            else Console.WriteLine("FALSE");
+        }
+    }
+}
