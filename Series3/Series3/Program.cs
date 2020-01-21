6c533d6b5a9d9f73ddeedb4d2528d5f4c69ed9eb
dex22044 <dex22044@yandex.ru>
2020-01-21 17:46:47 +0300

    Series3

---
+++ Series3/Series3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Series3
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                int res = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    res += Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine(res / 10);
+            }
+    }
+}
