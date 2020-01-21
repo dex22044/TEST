592c30688536edc7cfc26d9f306ae74434547944
dex22044 <dex22044@yandex.ru>
2020-01-21 17:44:02 +0300

    Series2

---
+++ Series2/Series2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Series2
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int res = 1;
+                for (int i = 0; i < 10; i++)
+                {
+                    res *= Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine(res);
+            }
+        }
+
+}
