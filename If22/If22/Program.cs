582749072b48cc4b991d8ecae8364f7491839961
dex22044 <dex22044@yandex.ru>
2020-01-21 17:07:00 +0300

    Сделал If22
    Сделал If22 (логично)

---
+++ If22/If22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace If22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
+            if (x == 0 || y == 0){
+                Console.WriteLine("X or Y equals 0"); return;
+            }
+
+            if (x > 0 && y > 0) Console.WriteLine("2");
+            else if (x < 0 && y > 0) Console.WriteLine("1");
+            else if (x > 0 && y < 0) Console.WriteLine("3");
+            else if (x < 0 && y < 0) Console.WriteLine("4");
+        }
+    }
+}
