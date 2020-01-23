d35f2c8dda84747b43db842fb1b111275b668600
dex22044 <dex22044@yandex.ru>
2020-01-23 17:14:10 +0300

    Array2

---
+++ Array2/Array2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Int64> powers = new List<Int64>();
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                powers.Add((Int64)Math.Pow(2, i));
+            }
+
+            foreach(Int64 num in powers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
