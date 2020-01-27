1a2826dde7f85f3989214c703909d76535799454
dex22044 <dex22044@yandex.ru>
2020-01-27 22:49:30 +0300

    Array80

---
+++ Array80/Array80/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array80
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> a = new List<int>();
+			var rand = new Random(new Random().Next(-10, 10));
+			int n = Convert.ToInt32(Console.ReadLine());
+			for (int i = 0; i < n; i++)
+			{
+				a.Add(rand.Next(-100000, 100000));
+			}
+
+			for(int i = 0; i < n-1; i++)
+			{
+				a[i] = a[i + 1];
+			}
+			a[n-1] = 0;
+			foreach (int i in a) Console.WriteLine(i);
+		}
+	}
+}
