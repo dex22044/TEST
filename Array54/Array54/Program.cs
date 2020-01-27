140d376916e8de21c469fe925142c613a27ec5a5
dex22044 <dex22044@yandex.ru>
2020-01-27 22:28:46 +0300

    Array54

---
+++ Array54/Array54/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array54
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> a = new List<int>();
+			int n = Convert.ToInt32(Console.ReadLine());
+			var rand = new Random(new Random().Next(-10, 10));
+			for (int i = 0; i < n; i++)
+			{
+				a.Add(rand.Next(-100000, 100000));
+			}
+
+			List<int> b = new List<int>();
+			foreach(int i in a)
+			{
+				if (i % 2 == 0) b.Add(i);
+			}
+			Console.WriteLine(b.Count);
+			foreach (int i in b) Console.WriteLine(i);
+		}
+	}
+}
