666d08846b3141d15cf05dad12628f8467724ec2
dex22044 <dex22044@yandex.ru>
2020-01-27 22:36:57 +0300

    Array58

---
+++ Array58/Array58/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array58
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> a = new List<int>();
+			List<int> b = new List<int>();
+			int n = Convert.ToInt32(Console.ReadLine());
+			var rand = new Random(new Random().Next(-10, 10));
+			for (int i = 0; i < n; i++)
+			{
+				a.Add(rand.Next(-100000, 100000));
+			}
+
+			for(int i = 0; i < n; i++)
+			{
+				int temp = 0;
+				for (int j = 0; j < i; j++) temp += a[j];
+				b.Add(temp);
+			}
+
+			foreach (int i in b) Console.WriteLine(i);
+		}
+	}
+}
