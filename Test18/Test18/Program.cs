8b27216a02d12750bed39e95729d3594c546f2c6
dex22044 <dex22044@yandex.ru>
2020-01-28 17:11:00 +0300

    Test

---
+++ Test18/Test18/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Primitives;
+
+namespace Test18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter c = new Counter(3);
+            c.add(5);
+            Cons.WriteLine(Convert.ToString(c.getCount()));
+        }
+    }
+}
