855c149e9f3170a836403ddd4f5047cbe613ec19
dex22044 <dex22044@yandex.ru>
2020-03-22 18:59:13 +0300

    AuthForm

--- Auth/Auth/AccessWindow.xaml.cs
+++ Auth/Auth/AccessWindow.xaml.cs
@@ -10,18 +10,60 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Auth
 {
     /// <summary>
-    /// Логика взаимодействия для AccessWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class AccessWindow : Window
     {
         public AccessWindow()
         {
             InitializeComponent();
+            users.Add(new User("admin", "newpass"));
+            users.Add(new User("АДМИН", "очки ннада?"));
+        }
+
+        class User
+        {
+            public string login;
+            public string password;
+            public User() { }
+            public User(string login, string password)
+            {
+                this.password = password;
+                this.login = login;
+            }
+        }
+
+        List<User> users = new List<User>();
+
+        private void Auth(object sender, RoutedEventArgs e)
+        {
+            int id = authorize(thisLogIn.Text, thisPassWord.Password);
+            if (id != -1)
+            {
+                Access.Content = "ACCESS GRANTED\nID:" + id;
+            }
+            else
+            {
+                Access.Content = "ACCESS DENIED";
+            }
+        }
+
+        int authorize(string log, string pass)
+        {
+            for (int i = 0; i < users.Count; i++)
+            {
+                if (users[i].login == log && users[i].password == pass)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
     }
 }
